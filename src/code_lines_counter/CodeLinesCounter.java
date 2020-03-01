03718f7fbe292f0a0542f0f08652984596b9b84d
alfoirazabal <49956896+alfoirazabal@users.noreply.github.com>
2020-03-01 16:45:05 -0300

    Setting Visual UI To OS Default

--- src/code_lines_counter/CodeLinesCounter.java
+++ src/code_lines_counter/CodeLinesCounter.java
@@ -6,6 +6,8 @@
 package code_lines_counter;
 
 import code_lines_counter.gui.FormMain;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -17,7 +19,20 @@ public class CodeLinesCounter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here      
+        // TODO code application logic here    
+        try {
+            // Set System L&F
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName()
+            );
+        } 
+        catch (
+                UnsupportedLookAndFeelException |
+                        ClassNotFoundException |
+                        InstantiationException |
+                        IllegalAccessException e
+        ) { }
+
         FormMain formMain = new FormMain();
         formMain.setLocationRelativeTo(null);
         formMain.setVisible(true);
